9996982149229ccd1681ad5c32ab757d3a568193
ucenik <ucenik@UCENIK_5_UCIONI>
2026-06-10 13:45:50 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bojer_mur_tabla
+{
+    internal class Program
+    {
+        static void BojerMur(string pattern)
+        {
+            int m = pattern.Length;
+            int[] tabelaSkokova = new int[256];
+
+            for (int i = 0; i < 256; i++)
+            {
+                tabelaSkokova[i] = m;
+            }
+
+            for (int i = 0; i < m - 1; i++)
+            {
+                tabelaSkokova[(byte)pattern[i]] = m - 1 - i;
+            }
+            int[] ispisano = new int[256];
+
+            for (int i = 0; i < m; i++)
+            {
+                char slovo = pattern[i];
+                if (ispisano[(byte)slovo] == 0)
+                {
+                    Console.WriteLine($"{slovo,-10}   {tabelaSkokova[(byte)slovo],-20}");
+                    ispisano[(byte)slovo] = 1;
+                }
+            }
+
+            Console.WriteLine($"{"*",-10}   {   m,-20}");
+        }
+        static void Main(string[] args)
+        {
+            string rec = Console.ReadLine();
+            BojerMur(rec);
+        }
+    }
+}
